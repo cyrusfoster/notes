ee6ca41ae44b1719674ad3ac28638c48701d1943
Cyrus <cyrus.foster@planet.com>
2018-02-21 16:06:43 -0800

    with legacy notes

--- make.sh
+++ make.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Make pdf_tex files from svgs
+# Todo: automate this for all svg files
+inkscape -D -z --file=svg/hyperbolic.svg --export-pdf=svg/hyperbolic.pdf --export-latex
+
 NAME=notes
 pdflatex $NAME.tex
 xdg-open $NAME.pdf
